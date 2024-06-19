c441b2d9692681046a60a189704ce8cc8def7935
srikanth-srinivas <srikanth.2001@outlook.com>
2024-06-19 14:02:58 +0530

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,17 @@
 import sys
-from sequence_reader import read_fasta
-from distance_matrix import calculate_distance_matrix
-from clustering import upgma
+from Bio import SeqIO
+from clustering import calculate_distance_matrix, upgma, get_newick_format
 from visualization import plot_tree
 
 def main(input_file, output_file):
-    sequences = read_fasta(input_file)
+    sequences = [str(record.seq) for record in SeqIO.parse(input_file, "fasta")]
     distance_matrix = calculate_distance_matrix(sequences)
-    tree = upgma(distance_matrix)
-    plot_tree(tree, output_file)
+    clusters, heights = upgma(distance_matrix)
+    root = max(clusters.keys())
+    newick_str = get_newick_format(clusters, heights, root)
+    plot_tree(newick_str, output_file)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python main.py <input_fasta_file> <output_image_file>")
-        sys.exit(1)
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     main(input_file, output_file)
